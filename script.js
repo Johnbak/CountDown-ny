93680ca4284147dba86d4584abb41ef140da6881
Johnbak <worstgrim@gmail.com>
2020-11-17 23:18:06 +0700

    1 file modified

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
   const SECOND = 1000; //1000 milisecond = 1 minute
   const MINUTE = SECOND * 60;
   const HOUR = MINUTE * 60;
-  const DAY = HOUR * 60;
+  const DAY = HOUR * 24;
 
   function setElementInnerText(id, text) {
     const element = document.getElementById(id);
@@ -11,6 +11,7 @@
 
   function countDown() {
     const now = new Date().getTime();
+    // const newYear = new Date(2020, 12, 31, 23, 59, 59, 00).getTime();
     const newYear = new Date("December 31, 2020 23:59:59").getTime();
     const unixTimeLeft = newYear - now;
 
